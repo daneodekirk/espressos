fd028f8d127c17786608adee08200c7403c25a49
Dane Odekirk <dizzzy@uw.edu>
2011-08-26 09:18:20 -0700

    Successful port to coffeescript

--- mobile.coffee.js
+++ mobile.coffee.js
@@ -1,5 +1,5 @@
 jQuery(function() {
-  var HEIGHT, RGBA, WIDTH, bluur, canvas, context, draw, img, loaded_imgs, log, motionblur, preload, total_imgs, weight, _i, _len, _ref;
+  var HEIGHT, RGBA, WIDTH, canvas, context, draw, img, loaded_imgs, log, motionblur, preload, total_imgs, weight, _i, _len, _ref;
   log = function(msg) {
     return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
   };
@@ -12,7 +12,7 @@ jQuery(function() {
       loaded_imgs.push(img);
       if (loaded_imgs.length === total_imgs) {
         imgdata = context.getImageData(0, 0, WIDTH, HEIGHT);
-        blured = bluur(imgdata);
+        blured = motionblur(imgdata, 3);
         context.putImageData(blured, 0, 0);
       }
     };
@@ -26,56 +26,52 @@ jQuery(function() {
     pixeldata[4 * row] = (pixeldata[row] + pixeldata[12 * row]) / 4;
     return pixeldata[8 * row] = (pixeldata[row] + pixeldata[16 * row]) / 5;
   };
-  motionblur = function() {
-    var blur, imagedata, pixeldata, pos, row, _fn;
-    blur = 0;
-    pos = 0;
-    imagedata = context.getImageData(0, 0, WIDTH, HEIGHT);
-    pixeldata = imagedata.data;
-    _fn = function() {
-      var pixel, _ref, _results;
-      blur = weight(pixeldata, row);
-      _results = [];
-      for (pixel = 3, _ref = pixeldata.length - 2; pixel <= _ref; pixel += 4) {
-        _results.push((function() {
-          blur = Math.max(0, blur - pixeldata[row + (pixel - 2)] + pixeldata[row + (pixel + 2)]);
-          return pixeldata[row + (pixel + 1)] = Math.floor(blur / 5);
-        })());
-      }
-      return _results;
-    };
-    for (row = 0; 0 <= HEIGHT ? row <= HEIGHT : row >= HEIGHT; 0 <= HEIGHT ? row++ : row--) {
-      _fn();
-    }
-    return context.putImageData(imagedata, 0, 0);
-  };
-  bluur = function(img, passes) {
-    var arr, h, i, im, inner, j, jump, k, n, outer, pos, rounds, step, w, _ref, _ref2, _ref3;
-    pos = 0;
-    rounds = passes || 1;
-    h = img.height;
+  motionblur = function(img, passes) {
+    var arr, h, i, im, inner, j, jump, k, m, n, outer, pos, rounds, step, w;
     w = img.width;
+    h = img.height;
     im = img.data;
-    for (n = 0, _ref = rounds - 1; 0 <= _ref ? n <= _ref : n >= _ref; 0 <= _ref ? n++ : n--) {
-      outer = h;
-      inner = w;
-      step = 4;
-      for (i = 0, _ref2 = outer - 1; 0 <= _ref2 ? i <= _ref2 : i >= _ref2; 0 <= _ref2 ? i++ : i--) {
-        jump = 4 * i;
-        for (k = 0; k <= 2; k++) {
-          pos = jump + k;
-          arr = 0;
-          for (j = 0; j <= 4; j++) {
-            arr += im[pos + step * j];
-          }
-          img[pos] = im[pos + step] = im[pos + step * 2] = Math.floor(arr / 5);
-          for (j = 3, _ref3 = inner - 3; 3 <= _ref3 ? j <= _ref3 : j >= _ref3; 3 <= _ref3 ? j++ : j--) {
-            arr = Math.max(0, arr - im[pos + (j - 2) * step] + im[pos + (j + 2) * step]);
-            im[pos + j * step] = Math.floor(arr / 5);
-          }
-          im[pos + j * step] = im[pos + (j + 1) * step] = Math.floor(arr / 5);
+    rounds = passes || 1;
+    pos = step = jump = inner = outer = arr = 0;
+    n = 0;
+    while (n < rounds) {
+      m = 0;
+      while (m < 1) {
+        if (m) {
+          outer = w;
+          inner = h;
+          step = w * 4;
+        } else {
+          outer = h;
+          inner = w;
+          step = 4;
         }
+        i = 0;
+        while (i < outer) {
+          jump = (m === 0 ? i * w * 4 : 4 * i);
+          k = 0;
+          while (k < 3) {
+            pos = jump + k;
+            j = arr = 0;
+            while (j < 5) {
+              arr += im[pos + step * j];
+              j++;
+            }
+            img[pos] = im[pos + step] = im[pos + step * 2] = Math.floor(arr / 5);
+            j = 3;
+            while (j < inner - 2) {
+              arr = Math.max(0, arr - im[pos + (j - 2) * step] + im[pos + (j + 2) * step]);
+              im[pos + j * step] = Math.floor(arr / 5);
+              j++;
+            }
+            im[pos + j * step] = im[pos + (j + 1) * step] = Math.floor(arr / 5);
+            k++;
+          }
+          i++;
+        }
+        m++;
       }
+      n++;
     }
     return img;
   };
--- mobile.js.coffee
+++ mobile.js.coffee
@@ -8,10 +8,9 @@ jQuery ->
     img.onload = -> 
       draw img
       loaded_imgs.push img
-      #motionblur() if loaded_imgs.length is total_imgs
       if loaded_imgs.length is total_imgs
         imgdata = context.getImageData 0, 0, WIDTH, HEIGHT
-        blured = bluur imgdata
+        blured = motionblur imgdata
         context.putImageData blured, 0, 0
         return
       
@@ -19,67 +18,48 @@ jQuery ->
 
   draw = (img) ->
     context.drawImage img, 0, 0, WIDTH, HEIGHT
-    #imgdata = context.getImageData 0, 0, WIDTH, HEIGHT
-    #log imgdata.data[1]
 
-  weight = (pixeldata, row) ->
-    pixeldata[row] = ( pixeldata[row] + pixeldata[4*row] + pixeldata[8*row] ) / 3
-    pixeldata[4*row] = ( pixeldata[row] + pixeldata[12*row] ) / 4
-    pixeldata[8*row] = ( pixeldata[row] + pixeldata[16*row] ) / 5
-
-  motionblur = ->
-    blur = 0
-    pos  = 0
-    imagedata = context.getImageData 0, 0, WIDTH, HEIGHT
-    pixeldata = imagedata.data
-
-    for row in [0..HEIGHT]
-      do ->
-        blur = weight pixeldata, row
-
-        for pixel in [3..pixeldata.length-2] by 4
-          do ->
-            blur = Math.max(0, blur - pixeldata[ row+( pixel-2 )] + pixeldata[row+( pixel+2 )])
-            pixeldata[ row + (pixel+1) ] = Math.floor(blur/5)
-
-    context.putImageData imagedata, 0, 0
-
-  bluur = (img, passes) ->
-    # Same as blur, but fakes borders for nicer code
-    # Increase passes for blurrier image
-    #var i, j, k, m, n, w = img.width, h = img.height, im = img.data,
-    #	rounds = passes || 0,
-    #	pos = step = jump = inner = outer = arr = 0;
-
-    pos = 0
-    rounds = passes or 1
-    h  = img.height
-    w  = img.width
+  motionblur = (img, passes) ->
+    w = img.width
+    h = img.height
     im = img.data
-
-    for n in [0..rounds-1]
-        # Row blurring
-        outer = h
-        inner = w
-        step = 4
-        
-        for i in [0..outer-1]
-            jump = 4*i
-            for k in [0..2]
-                pos = jump+k
-                arr = 0
-
-                for j in [0..4]
-                    arr += im[pos+step*j]
-
-                img[pos] = im[pos+step] = im[pos+step*2] = Math.floor(arr/5) # Slightly wrong, but nicer to read
-                for j in [3..inner-3]
-                    arr = Math.max(0, arr - im[pos+(j-2)*step] + im[pos+(j+2)*step])
-                    im[pos+j*step] = Math.floor(arr/5)
-                # j is now inner - 2 (1 bigger)
-                im[pos+j*step] = im[pos+(j+1)*step] = Math.floor(arr/5) # Slightly wrong, but nicer to read
-    return img
-
+    rounds = passes or 1
+    pos = step = jump = inner = outer = arr = 0
+    n = 0
+    while n < rounds
+      m = 0
+      while m < 2
+        if m
+          outer = w
+          inner = h
+          step = w * 4
+        else
+          outer = h
+          inner = w
+          step = 4
+        i = 0
+        while i < outer
+          jump = (if m == 0 then i * w * 4 else 4 * i)
+          k = 0
+          while k < 3
+            pos = jump + k
+            j = arr = 0
+            while j < 5
+              arr += im[pos + step * j]
+              j++
+            img[pos] = im[pos + step] = im[pos + step * 2] = Math.floor(arr / 5)
+            j = 3
+            while j < inner - 2
+              arr = Math.max(0, arr - im[pos + (j - 2) * step] + im[pos + (j + 2) * step])
+              im[pos + j * step] = Math.floor(arr / 5)
+              j++
+            im[pos + j * step] = im[pos + (j + 1) * step] = Math.floor(arr / 5)
+            k++
+          i++
+        m++
+      n++
+    img
+  
 
     
 
