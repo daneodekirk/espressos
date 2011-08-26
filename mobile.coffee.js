0f4f08bb2b7c02d1574854a1c0ba89c9f8e2f65c
Dane Odekirk <dizzzy@uw.edu>
2011-08-26 08:27:25 -0700

    Another failed attempt

--- mobile.coffee.js
+++ mobile.coffee.js
@@ -1,5 +1,5 @@
 jQuery(function() {
-  var HEIGHT, RGBA, WIDTH, canvas, context, draw, img, log, preload, _i, _len, _ref;
+  var HEIGHT, RGBA, WIDTH, canvas, context, draw, img, loaded_imgs, log, motionblur, preload, total_imgs, weight, _i, _len, _ref;
   log = function(msg) {
     return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
   };
@@ -7,7 +7,11 @@ jQuery(function() {
     var img;
     img = new Image;
     img.onload = function() {
-      return draw(img);
+      draw(img);
+      loaded_imgs.push(img);
+      if (loaded_imgs.length === total_imgs) {
+        return motionblur();
+      }
     };
     return img.src = src;
   };
@@ -17,9 +21,39 @@ jQuery(function() {
     imgdata = context.getImageData(0, 0, WIDTH, HEIGHT);
     return log(imgdata.data[1]);
   };
+  weight = function(pixeldata, row) {
+    pixeldata[row] = (pixeldata[row] + pixeldata[4 * row] + pixeldata[8 * row]) / 3;
+    pixeldata[4 * row] = (pixeldata[row] + pixeldata[12 * row]) / 4;
+    return pixeldata[8 * row] = (pixeldata[row] + pixeldata[16 * row]) / 5;
+  };
+  motionblur = function() {
+    var blur, imagedata, pixeldata, pos, row, _fn;
+    blur = 0;
+    pos = 0;
+    imagedata = context.getImageData(0, 0, WIDTH, HEIGHT);
+    pixeldata = imagedata.data;
+    _fn = function() {
+      var pixel, _ref, _results;
+      blur = weight(pixeldata, row);
+      _results = [];
+      for (pixel = 3, _ref = pixeldata.length - 2; pixel <= _ref; pixel += 4) {
+        _results.push((function() {
+          blur = Math.max(0, blur - pixeldata[row + (pixel - 2)] + pixeldata[row + (pixel + 2)]);
+          return pixeldata[row + (pixel + 1)] = Math.floor(blur / 5);
+        })());
+      }
+      return _results;
+    };
+    for (row = 0; 0 <= HEIGHT ? row <= HEIGHT : row >= HEIGHT; 0 <= HEIGHT ? row++ : row--) {
+      _fn();
+    }
+    return context.putImageData(imagedata, 0, 0);
+  };
   WIDTH = 320;
   HEIGHT = 240;
   RGBA = ['RED', 'BLUE'];
+  loaded_imgs = [];
+  total_imgs = $('img').length;
   canvas = $('#tiles').get(0);
   context = canvas.getContext('2d');
   _ref = $('img');
--- mobile.js.coffee
+++ mobile.js.coffee
@@ -5,7 +5,11 @@ jQuery ->
 
   preload = (src) ->
     img        = new Image
-    img.onload = -> draw img
+    img.onload = -> 
+      draw img
+      loaded_imgs.push img
+      motionblur() if loaded_imgs.length is total_imgs
+      
     img.src    = src
 
   draw = (img) ->
@@ -13,9 +17,33 @@ jQuery ->
     imgdata = context.getImageData 0, 0, WIDTH, HEIGHT
     log imgdata.data[1]
 
+  weight = (pixeldata, row) ->
+    pixeldata[row] = ( pixeldata[row] + pixeldata[4*row] + pixeldata[8*row] ) / 3
+    pixeldata[4*row] = ( pixeldata[row] + pixeldata[12*row] ) / 4
+    pixeldata[8*row] = ( pixeldata[row] + pixeldata[16*row] ) / 5
+
+  motionblur = ->
+    blur = 0
+    pos  = 0
+    imagedata = context.getImageData 0, 0, WIDTH, HEIGHT
+    pixeldata = imagedata.data
+
+    for row in [0..HEIGHT]
+      do ->
+        blur = weight pixeldata, row
+
+        for pixel in [3..pixeldata.length-2] by 4
+          do ->
+            blur = Math.max(0, blur - pixeldata[ row+( pixel-2 )] + pixeldata[row+( pixel+2 )])
+            pixeldata[ row + (pixel+1) ] = Math.floor(blur/5)
+
+    context.putImageData imagedata, 0, 0
+
   WIDTH  = 320
   HEIGHT = 240
   RGBA   = ['RED', 'BLUE']
+  loaded_imgs = []
+  total_imgs = $('img').length
   
   canvas  = $('#tiles').get 0
   context = canvas.getContext '2d'
@@ -23,5 +51,3 @@ jQuery ->
   preload img.src for img in $('img')
 
   return
-
-
